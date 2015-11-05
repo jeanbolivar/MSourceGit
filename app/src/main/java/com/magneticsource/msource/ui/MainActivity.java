81efdda9daa8b9288bf0dd162b3c3e274212117f
tony <3tonycesar@gmail.com>
2015-11-05 12:01:10 -0500

    subiendo interfaz grafica

--- app/src/main/java/com/magneticsource/msource/ui/MainActivity.java
+++ app/src/main/java/com/magneticsource/msource/ui/MainActivity.java
@@ -1,31 +1,39 @@
-package com.magneticsource.msource;
+package com.magneticsource.msource.ui;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.magneticsource.msource.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View arg0) {
+                EditText e =(EditText) findViewById(R.id.editText);
+                Intent intent;
+                if(e.getText().length() == 1)
+                intent=   new Intent(getApplicationContext(), ProfesorActivity.class);
+                else
+                intent = new Intent(getApplicationContext(), AlumnoActivity.class);
+                startActivity(intent);
+
             }
+
         });
+
     }
 
     @Override
--- app/src/main/java/com/magneticsource/msource/ui/ProfesorActivity.java
+++ app/src/main/java/com/magneticsource/msource/ui/ProfesorActivity.java
@@ -1,37 +1,42 @@
-package com.magneticsource.msource;
+package com.magneticsource.msource.ui;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.magneticsource.msource.R;
+
+public class ProfesorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_profesor);
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View arg0) {
+
+                Intent intent;
+
+                intent = new Intent(getApplicationContext(), CapturarActivity.class);
+
+                startActivity(intent);
+
             }
+
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_profesor, menu);
         return true;
     }
 
