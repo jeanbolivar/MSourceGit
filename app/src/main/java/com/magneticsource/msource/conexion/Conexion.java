dcdd1ae0c0138940b301cb2ce4cdda4cd6fb5e94
Jean <jeango_152@hotmail.com>
2015-11-05 20:14:29 -0500

    Clase conexion de prueba

---
+++ app/src/main/java/com/magneticsource/msource/conexion/Conexion.java
@@ -0,0 +1,7 @@
+package com.magneticsource.msource.conexion;
+
+/**
+ * Created by JeanManuel on 05/11/2015.
+ */
+public class Conexion {
+}
---
+++ app/src/main/java/com/magneticsource/msource/control/Alumno.java
@@ -0,0 +1,10 @@
+package com.magneticsource.msource.control;
+
+/**
+ * Created by tony on 02/11/15.
+ */
+public class Alumno extends Persona{
+    public String enviarInformacion() {
+        return getDni();
+    }
+}
